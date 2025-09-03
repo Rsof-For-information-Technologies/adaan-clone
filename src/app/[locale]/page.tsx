4090ab349de2c52ce4af224a833d2abb1b6e6717
Talha-Hamayoun175 <talha.humayun@nestosh.com>
2025-09-03 18:46:39 +0500

    again imports

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,14 +1,14 @@
 import Footer from "@/app/footer/Footer";
 import AboutUsSection from "@/components/AboutUsSection";
-import { BlogsSection } from "@/components/BlogsSection";
 import ContentKeyFactsText from "@/components/ContentKeyFactsText";
 import DiscoverSection from "@/components/DiscoverSection";
 import HeroSlider from "@/components/HeroSlider";
-import { MagazineSection } from "@/components/MagazineSection";
-import { SustainabilitySection } from "@/components/SustainabilitySection";
 import VideoSection from "@/components/VideoSection";
 import WhatWebelieveSlider from "@/components/WhatWebelieveSection";
 import Header from "../header/Header";
+import { MagazineSection } from "@/components/MagazineSection";
+import { SustainabilitySection } from "@/components/SustainabilitySection";
+import { BlogsSection } from "@/components/BlogsSection";
 
 export default async function Home() {
 
